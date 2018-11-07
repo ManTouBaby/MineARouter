2789bc7bed3950faf39398a3d5eb0dba8e7205cc
GitTest <https://github.com/ManTouBaby/Test.git>
2018-11-07 22:57:33 +0800

    11.07-添加基类

--- book/src/main/java/com/hrw/book/bk/service/IBKHomePage.java
+++ book/src/main/java/com/hrw/book/bk/service/IBKHomePage.java
@@ -1,9 +1,19 @@
 package com.hrw.book.bk.service;
 
+import android.database.Observable;
+
+import com.hrw.book.bk.entity.HomePageBO;
+
+import retrofit2.http.POST;
+
 /**
  * @author:MtBaby
  * @date:2018/11/07 22:42
  * @desc:
  */
 public interface IBKHomePage {
+    @POST("/resource/homepage/check")
+    Observable<HomePageBO> getHomePageData();
+
+    
 }
